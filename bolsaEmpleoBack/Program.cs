73fca2f7305be12b3dbd34a87529f8427f3748ca
julianrealpe93 <julianrealpe93@gmail.com>
2024-02-20 10:54:59 -0500

    actualizacion de cors: 20/02/2024

--- bolsaEmpleoBack/Program.cs
+++ bolsaEmpleoBack/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
         policy =>
         {
-            policy.WithOrigins("*");
+            policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod(); ;
         });
 });
 
